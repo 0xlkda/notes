36838528a9657e997adf7a910834fba665f4ff3c
0xlkda <lekhacduyanh@gmail.com>
2023-11-11 16:52:09 +0700

    implement interactive

--- docs/latest/3d.js
+++ docs/latest/3d.js
@@ -1,3 +1,5 @@
+var PI = Math.PI
+var toRadians = (degree) => degree * PI / 180
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
@@ -8,27 +10,6 @@ function Point3D({x, y, z, color, label}) {
   this.z = z
   this.color = color
   this.label = label || ''
-}
-
-function rotateX(point, angle) {
-  var angle = angle * Math.PI / 180
-  var y = point.y * Math.cos(angle) - point.z * Math.sin(angle)
-  var z = point.y * Math.sin(angle) + point.z * Math.cos(angle)
-  return new Point3D({ ...point, y, z })
-}
-
-function rotateY(point, angle) {
-  var angle = angle * Math.PI / 180
-  var z = point.z * Math.cos(angle) - point.x * Math.sin(angle)
-  var x = point.z * Math.sin(angle) + point.x * Math.cos(angle)
-  return new Point3D({ ...point, x, z })
-}
-
-function rotateZ(point, angle) {
-  var angle = angle * Math.PI / 180
-  var x = point.x * Math.cos(angle) - point.y * Math.sin(angle)
-  var y = point.x * Math.sin(angle) + point.y * Math.cos(angle)
-  return new Point3D({ ...point, x, y })
 }
 
 function draw2DPoint(point) {
@@ -42,7 +23,7 @@ function draw2DPoint(point) {
   } else {
     ctx.strokeStyle = point.color
     ctx.beginPath()
-    ctx.arc(x, y, 2, 0, 2 * Math.PI)
+    ctx.arc(x, y, 2, 0, 2 * PI)
     ctx.stroke()
   }
   ctx.restore()
@@ -69,25 +50,13 @@ function draw2DLine(p1, p2, showPoint = true) {
   ctx.restore()
 }
 
-var CENTER = new Point3D({
+var VIEW_CENTER = new Point3D({
   x:canvas.width / 2, 
   y:canvas.width / 2, 
   z:0
 })
-
-function translate(point, to) {
-  var x = point.x + to.x
-  var y = point.y + to.y
-  var z = point.z + to.z
-  return new Point3D({...point, x, y, z})
-}
-
-function scale(point, ratio) {
-  var x = point.x * ratio
-  var y = point.y * ratio
-  var z = point.z * ratio
-  return new Point3D({...point, x, y, z})
-}
+VIEW_CENTER.type = 'VIEW_CENTER'
+VIEW_CENTER.color = 'gray'
 
 function makeCube(origin) {
   var p1 = new Point3D({x:-1.0, y:-1.0, z: 1.0, label: 'p1'})
@@ -118,10 +87,8 @@ function drawCube(angle, position) {
   var cubeTranslated = []
 
   function vertexProcessing(vertex) {
-    vertex = scale(vertex, 50)
-    vertex = rotateX(vertex, -angle)
-    vertex = rotateY(vertex, -angle)
-    vertex = rotateZ(vertex, 0)
+    vertex = scale(vertex, 100)
+    vertex = rotate(vertex, angle)
     vertex = translate(vertex, origin)
     return vertex
   }
@@ -155,11 +122,266 @@ function drawFace(p1, p2, p3, p4) {
   draw2DLine(p4, p1, false)
 }
 
-function loop(dt) {
-  var angle = (dt / 10) % 360
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  drawCube(angle, CENTER)
-  requestAnimationFrame(loop)
+
+var Vec3 = (x, y, z) => {
+  var vec = [x, y, z]
+  vec.x = x
+  vec.y = y
+  vec.z = z
+  vec.type = 'VEC3'
+  vec.at = (index) => vec[index]
+  vec.setAt = (index, value) => (vec[index] = value)
+  return vec
 }
 
-loop()
+var Mat3 = (a, b, c, d, e, f, g, h, j) => {
+  var mat = [
+    ...Vec3(a, b, c),
+    ...Vec3(d, e, f),
+    ...Vec3(g, h, j),
+  ]
+
+  mat.type = 'MAT3'
+  mat.at = (y, x) => mat[y * 3 + x]
+  mat.setAt = (y, x, value) => (mat[y * 3 + x] = value)
+  mat.multiply_vec3 = (to) => {
+    return Vec3(
+      mat.at(0, 0) * to.x + mat.at(0, 1) * to.x + mat.at(0, 2) * to.x,
+      mat.at(1, 0) * to.y + mat.at(1, 1) * to.y + mat.at(1, 2) * to.y,
+      mat.at(2, 0) * to.z + mat.at(2, 1) * to.z + mat.at(2, 2) * to.z,
+    )
+  }
+  return mat
+}
+
+var cos = Math.cos
+var sin = Math.sin
+
+var MAT3_ZERO = Mat3(
+  1, 0, 0,
+  0, 1, 0,
+  0, 0, 1,
+)
+
+var MAT3_TRANSLATE = (to) => Mat3(
+  1, 0, to.x,
+  0, 1, to.y,
+  0, 0, 1,
+)
+
+var MAT3_ROTATE_Z = (theta) => Mat3(
+  cos(theta), -sin(theta), 0,
+  sin(theta),  cos(theta), 0,
+  0, 0, 1,
+)
+
+var MAT3_ROTATE_X = (theta) => Mat3(
+  1, 0, 0,
+  0, cos(theta), -sin(theta),
+  0, sin(theta),  cos(theta),
+)
+
+var MAT3_ROTATE_Y = (theta) => Mat3(
+  cos(theta), 0, sin(theta),
+  0, 1, 0,
+  -sin(theta), 0, cos(theta)
+)
+
+function product_mat3_mat3(left, right) {
+  var [x0, y0, z0] = multiply_mat3_vec3(left, Vec3(right.at(0, 0), right.at(1, 0), right.at(2, 0)))
+  var [x1, y1, z1] = multiply_mat3_vec3(left, Vec3(right.at(0, 1), right.at(1, 1), right.at(2, 1)))
+  var [x2, y2, z2] = multiply_mat3_vec3(left, Vec3(right.at(0, 2), right.at(1, 2), right.at(2, 2)))
+  return Mat3(
+    x0, x1, x2,
+    y0, y1, y2,
+    z0, z1, z2,
+  )
+}
+
+function multiply_mat3_vec3(from, to) {
+  var x = from.at(0, 0) * to.x + from.at(0, 1) * to.y + from.at(0, 2) * to.z
+  var y = from.at(1, 0) * to.x + from.at(1, 1) * to.y + from.at(1, 2) * to.z 
+  var z = from.at(2, 0) * to.x + from.at(2, 1) * to.y + from.at(2, 2) * to.z 
+  return [x, y, z]
+}
+
+var scale = (point, ratio) => {
+  var [x, y, z] = multiply_mat3_vec3(MAT3_ZERO, point).map(x => x * ratio)
+  return new Point3D({...point, x, y, z})
+}
+
+var rotate = (point, angle) => {
+  var radianX = toRadians(angle + angleX)
+  var radianY = toRadians(angle + angleY)
+  var radianZ = toRadians(angle + angleZ)
+
+  var XY = product_mat3_mat3(MAT3_ROTATE_X(radianX), MAT3_ROTATE_Y(radianY))
+  var XYZ = product_mat3_mat3(MAT3_ROTATE_Z(radianZ), XY)
+  var [x, y, z] = multiply_mat3_vec3(XYZ, point)
+  return new Point3D({...point, x, y, z})
+}
+
+var translate = (from, to) => {
+  var [x, y, z] = multiply_mat3_vec3(MAT3_TRANSLATE(from), to)
+  var x = from.x + x
+  var y = from.y + y
+  var z = from.z + z
+  return new Point3D({...from, x, y, z})
+}
+
+var angle = 0
+var angleX = 0
+var angleY = 0
+var angleZ = 0
+var translatedX = VIEW_CENTER.x
+var translatedY = VIEW_CENTER.y
+var translatedZ = VIEW_CENTER.z
+
+function loop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  var PIVOT_POINT = Vec3(0, 0, 0)
+  PIVOT_POINT.label = 'PIVOT'
+  PIVOT_POINT.color = 'pink'
+
+  var MARKER = Vec3(translatedX, translatedY, translatedZ)
+  PIVOT_POINT = translate(PIVOT_POINT, MARKER)
+
+  draw2DPoint(VIEW_CENTER)
+  draw2DPoint(PIVOT_POINT)
+  drawCube(angle, PIVOT_POINT)
+  // requestAnimationFrame(loop)
+}
+
+loop(0)
+
+var lastMouseX = 0, lastMouseY = 0
+canvas.addEventListener('mousedown', (ev) => {
+  lastMouseX = ev.clientX
+  lastMouseY = ev.clientY
+  canvas.style.cursor = 'grabbing'
+  canvas.addEventListener('mousemove', mouseMoved)
+})
+
+canvas.addEventListener('mouseup', () => {
+  canvas.style.cursor = 'pointer'
+  canvas.removeEventListener('mousemove', mouseMoved)
+})
+
+function mouseMoved(ev) {
+  var deltaX = ev.pageX - lastMouseX
+  var deltaY = ev.pageY - lastMouseY
+
+  lastMouseX = ev.pageX
+  lastMouseY = ev.pageY
+
+  angleY -= deltaX * -0.5
+  angleX += deltaY * -0.5
+
+  loop()
+}
+
+document.addEventListener('keypress', (e) => {
+  switch (e.keyCode) {
+  
+  // TRANSLATE X
+  case 97: // A
+    translatedX -= 2
+    break
+
+  case 100: // D
+    translatedX += 2
+    break
+
+  // TRANSLATE Y
+  case 119: // W
+    translatedY -= 2
+    break
+
+  case 115: // S
+    translatedY += 2
+    break
+
+  // TRANSLATE Z
+  case 61: // + ZOOM IN
+    translatedZ += 0.1
+    break
+
+  case 45: // - ZOOM OUT
+    translatedZ -= 0.1
+    break
+
+  // ROTATE X
+  case 105: // I
+    angleX += 1
+    break
+
+  case 107: // K
+    angleX -= 1
+    break
+
+  // ROTATE Y
+  case 106: // J
+    angleY -= 1
+    break
+
+  case 108: // L
+    angleY += 1
+    break
+
+  // ROTATE Z
+  case 117: // U
+    angleZ -= 1
+    break
+
+  case 111: // O
+    angleZ += 1
+    break
+
+  default:
+    break
+  }
+
+  loop()
+})
+
+// TEST
+test('should do the product 3x3 correctly', () => {
+  var left = Mat3(
+    2, 7, 3,
+    1, 5, 8,
+    0, 4, 1
+  )
+
+  var right = Mat3(
+    3, 0, 1,
+    2, 1, 0,
+    1, 2, 4
+  )
+
+  var p = product_mat3_mat3(left, right)
+  expectPairEqual(
+    p.at(0, 0), 23,
+    p.at(0, 1), 13,
+    p.at(0, 2), 14,
+    p.at(1, 0), 21,
+    p.at(1, 1), 21,
+    p.at(1, 2), 33,
+    p.at(2, 0), 9,
+    p.at(2, 1), 6,
+    p.at(2, 2), 4,
+  )
+})
+
+function test(name, tester) {
+  tester()
+  console.log(`${name}: OK!`)
+}
+
+function expectPairEqual(...args) {
+  for (let index = 0; index < args.length; index+=2) {
+    const left = args[index]
+    const right = args[index+1]
+    if (left !== right) throw new Error(`Expect: ${left} === ${right}`)
+  }
+}
